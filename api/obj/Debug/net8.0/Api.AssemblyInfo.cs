ae1a4098475b6f751d608e8a3a10b4d433ef26aa
AEIL2203 <aibanezl@miumg.edu.gt>
2025-09-29 22:54:32 -0600

    Actualización de producción

--- api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3fa03e34d0ef588cc47d00588429bd8399fd585")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+245eb19eb1a56c13a08e274b25aa89e472f67899")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
